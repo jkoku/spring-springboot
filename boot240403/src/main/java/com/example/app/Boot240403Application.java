c3fc7443f452d75ae8891a3ebf24bc3f16e19b33
jkoku <jinkyounggg@gmail.com>
2024-04-03 17:14:55 +0900

    240403..

---
+++ boot240403/src/main/java/com/example/app/Boot240403Application.java
@@ -0,0 +1,15 @@
+package com.example.app;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan(basePackages="com.yedam.**.mapper")
+public class Boot240403Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boot240403Application.class, args);
+	}
+	
+}
